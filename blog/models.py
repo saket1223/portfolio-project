aef0c27e270c4d817fefa04eebcf2941c8d3cc13
SKY <saketkumaryadav@gmail.com>
2020-01-05 17:50:17 +0530

    finish portfolio job and blog module

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,14 @@ class Blog(models.Model):
 
     #Add the Blog app to the settings
     # Create a migration
+    #python manage.py makemigrations
     # Migrate
+    #python manage.py migrate
     # Add to the admin 
+
+    def summary(self):
+        return self.body[:100]
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
